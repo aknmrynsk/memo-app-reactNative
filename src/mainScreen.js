6cb8e878fdc405bb965a5866f19168019d0541bd
aknmrynsk <akanuma.ryunosuke.83z@st.kyoto-u.ac.jp>
2024-04-24 18:17:10 +0900

    decide the data structure and arranged memos.

--- src/mainScreen.js
+++ src/mainScreen.js
@@ -1,20 +1,48 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Title } from "react-native-paper";
+import { StyleSheet, View, FlatList } from "react-native";
+import { List } from "react-native-paper";
+
+const memos = [
+    {
+        text: 'メモ1',
+        createdAt: 1713948778000
+    },
+    {
+        text: 'メモ2',
+        createdAt: 1713948778010
+    },
+    {
+        text: 'メモ3',
+        createdAt: 1713948778100
+    },
+    {
+        text: 'メモ4',
+        createdAt: 1713948778200
+    }
+]
 
 export const MainScreen = () => {
     return (
-        <View style={style.container}>
-            <Title>ここはメイン画面です。</Title>
+        <View style={styles.container}>
+            <FlatList
+                style={styles.list}
+                data={memos}
+                keyExtractor={item => `${item.createdAt}`}
+                renderItem={({ item }) => (
+                    <List.Item
+                        title={item.text}
+                        description={item.createdAt} />
+                )}
+            />
         </View>
     );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+    },
+    list: {
+        flex: 1,
+    },
 });
